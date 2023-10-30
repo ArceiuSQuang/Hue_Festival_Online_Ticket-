9f3630a18839d75ccedc5ff8700847207b1ab26c
Le Quang <arceiusgaming@gmail.com>
2023-10-30 15:22:09 +0700

    Add project files.

---
+++ Hue_Festival_Online_Ticket/Program.cs
@@ -0,0 +1,43 @@
+using Hue_Festival_Online_Ticket.Context;
+using Hue_Festival_Online_Ticket.IService;
+using Hue_Festival_Online_Ticket.Service;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//conect db
+builder.Services.AddDbContext<Hue_Festival_Context>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("strConnect"));
+
+}
+
+);
+//register service
+builder.Services.AddScoped<IChuongtrinhService, ChuongtrinhService>();
+builder.Services.AddScoped<IDiadiemService, DiadiemService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,4 @@
+# Hue_Festival_Online_Ticket
+ERDD
+
+![Hue_Festival_ERD_V0](https://github.com/TRONGTIN0803/Hue_Festival_Online_Ticket/assets/110965574/22f81dff-8d16-47d2-b5b5-acb08b66df4d)
